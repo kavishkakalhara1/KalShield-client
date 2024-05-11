78d53e600ac7f805aa796f8828c66389c9a5e7ac
Kavishka Kalhara <kavikalhara9h@gmail.com>
2024-05-12 00:19:47 +0530

    Added scrolling animations to the home page components

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,42 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import SignIn from './pages/SignIn';
-import Dashboard from './pages/Dashboard';
-import Projects from './pages/Projects';
-import SignUp from './pages/SignUp';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import PrivateRoute from './components/PrivateRoute';
-import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
-import CreatePost from './pages/CreatePost';
-import UpdatePost from './pages/UpdatePost';
-import PostPage from './pages/PostPage';
-import ScrollToTop from './components/ScrollToTop';
-import Search from './pages/Search';
-import NotFound from './pages/NotFound';
-import React, { Suspense } from 'react';
-import AnimatedRoutes from './components/Animatedroutes';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import PrivateRoute from "./components/PrivateRoute";
+import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
+import CreatePost from "./pages/CreatePost";
+import UpdatePost from "./pages/UpdatePost";
+import PostPage from "./pages/PostPage";
+import ScrollToTop from "./components/ScrollToTop";
+import Search from "./pages/Search";
+import NotFound from "./pages/NotFound";
+import React, { Suspense } from "react";
+import AnimatedRoutes from "./components/Animatedroutes";
 
 export default function App() {
   // const location = useLocation();
   return (
-    
     <BrowserRouter>
       <ScrollToTop />
       <Header />
       <AnimatedRoutes />
       <Routes>
-        
-        <Route path='/post/:postSlug' element={<PostPage />} />
+        <Route path="/post/:postSlug" element={<PostPage />} />
         <Route element={<PrivateRoute />}>
-          <Route path='/dashboard' element={<Dashboard />} />
-          <Route path='/create-post' element={<CreatePost />} />
-          <Route path='/update-post/:postId' element={<UpdatePost />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:postId" element={<UpdatePost />} />
         </Route>
         <Route element={<OnlyAdminPrivateRoute />}>
           {/* Add admin routes here if needed */}
         </Route>
         {/* Define a catch-all route for 404 errors */}
-        <Route path='*' element={<NotFound />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
     </BrowserRouter>
--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,5 +1,6 @@
 import { Card } from "flowbite-react";
 import React from "react";
+import { motion } from "framer-motion";
 
 export default function ContactCard() {
   return (
@@ -20,20 +21,22 @@ export default function ContactCard() {
             Computer Engineering Undergraduate
           </span>
           <div className="flex mt-4 space-x-3 lg:mt-6">
-            <a
+            <motion.a
               href="https://wa.me/94701117791"
               className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-green-500 rounded-lg hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
               target="_blank"
+              whileTap={{ scale: 0.85 }}
             >
               WhatsApp
-            </a>
-            <a
+            </motion.a>
+            <motion.a
               href="https://www.linkedin.com/in/kavishkakalhara/"
               className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:hover:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
               target="_blank"
+              whileTap={{ scale: 0.85 }}
             >
               LinkedIn
-            </a>
+            </motion.a>
           </div>
         </div>
       </Card>
